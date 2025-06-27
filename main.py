f2d1cdc1c5c0515c75a033d89a21f7a7b2ed27d6
AkHydros <akatzighera@gmail.com>
2025-06-27 00:49:36 -0400

    B: Updated Code

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from stats import words_string
-words_string()
+from stats import words_string, count_occurence
 
+print('============ BOOKBOT ============')
+print('Analyzing book found at books/frankenstein.txt...')
+print('----------- Word Count ----------')
+words_string()
+print('--------- Character Count -------')
+count_occurence()
+print('============= END ===============')
--- stats.py
+++ stats.py
@@ -8,5 +8,24 @@ def get_book_text(filepath):
 def words_string():
     books_text = get_book_text('books/frankenstein.txt')
     words = books_text.split()
-    print (f'{len(words)} words found in the document')
+    print (f'Found {len(words)} total words')
+
+def count_occurence():
+    books_text = get_book_text('books/frankenstein.txt')
+    characters = {}
+
+#conversion to lowe case to count
+    books_text = books_text.lower()
+
+#counter function
+    for char in books_text:
+        if char.isalpha():
+            characters[char] = characters.get(char,0) + 1
+
+#print results
+    for char, count in sorted(characters.items(), key= lambda x:x[1], reverse=True):
+        print(f"{char}: {count}")
+
+
+
 
